7047a295f9c0405d58be73f73af976c33fccd64d
Phill Barron <phillipbarron242@gmail.com>
2019-12-21 11:09:59 +0000

    formatting

--- helloworld.ts
+++ helloworld.ts
@@ -1,21 +1,20 @@
-
-
 interface Tag {
-    description: String,
-    url: String
+  description: String;
+  url: String;
 }
+
 let message: String = "So this is typescript";
 
-console.log('message', JSON.stringify(message, null, 2));
+console.log("message", JSON.stringify(message, null, 2));
 
-const getDescription = (tags: Array<Tag>) : Array<String> => {
-    return tags.map(tag => tag.description);
-}
+const getDescription = (tags: Array<Tag>): Array<String> => {
+  return tags.map(tag => tag.description);
+};
 
 const tag1 = {
-    description: "some tags",
-    url: "http://a.b.c/meh",
-}
+  description: "some tags",
+  url: "http://a.b.c/meh"
+};
 
 const someTagsArray: Array<Tag> = [tag1];
 
