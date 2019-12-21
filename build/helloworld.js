70f324d0fa197b473250f528ad0e787ff73cff5f
Phill Barron <phillipbarron242@gmail.com>
2019-12-21 11:09:25 +0000

    interface testing

--- build/helloworld.js
+++ build/helloworld.js
@@ -1,2 +1,11 @@
 var message = "So this is typescript";
 console.log('message', JSON.stringify(message, null, 2));
+var getDescription = function (tags) {
+    return tags.map(function (tag) { return tag.description; });
+};
+var tag1 = {
+    description: "some tags",
+    url: "http://a.b.c/meh",
+};
+var someTagsArray = [tag1];
+console.log(getDescription(someTagsArray));
--- helloworld.ts
+++ helloworld.ts
@@ -1,3 +1,22 @@
+
+
+interface Tag {
+    description: String,
+    url: String
+}
 let message: String = "So this is typescript";
 
 console.log('message', JSON.stringify(message, null, 2));
+
+const getDescription = (tags: Array<Tag>) : Array<String> => {
+    return tags.map(tag => tag.description);
+}
+
+const tag1 = {
+    description: "some tags",
+    url: "http://a.b.c/meh",
+}
+
+const someTagsArray: Array<Tag> = [tag1];
+
+console.log(getDescription(someTagsArray));
